a6606ee7edc07c2796cb3c3b6767537b02c03c8a
Yu Ricky <leoleo5994@gmail.com>
2022-07-04 17:32:16 +0800

    quick-start-template init

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { fileURLToPath, URL } from "url";
+import svgLoader from "vite-svg-loader";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    svgLoader(),
+    Components({
+      dts: true,
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+        /\.vue$/,
+        /\.vue\?vue/,
+        /\.md$/, //
+      ],
+      imports: ["vue", "vue-router"],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(jpe?g|png|svg|gif)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.(css$)/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
+});
