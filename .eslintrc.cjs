f75f2eaaf41c0a00ca7c69d9c2b66a7fe622761a
Yu Ricky <leoleo5994@gmail.com>
2022-07-05 11:18:42 +0800

    update vite.config.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "./.eslintrc-auto-import.json",
   ],
   rules: {
     "vue/no-unused-vars": "error",
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       dts: true,
     }),
     AutoImport({
+      dts: true,
       include: [
         /\.[tj]sx?$/,
         /\.vue$/,
